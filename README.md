e4764af85ab9dd76c8d8938a60e0719563a3da96
jacksop <pjackson@mbs-projects.com>
2014-10-04 15:15:55 +0100

    Output total hours by year and month
    * use clj-time to work out time duration
    * use clojure.pprint/print-table to format output

--- README.md
+++ README.md
@@ -1,14 +1,39 @@
 # timetotal
 
-A Clojure library designed to ... well, that part is up to you.
+A Clojure command line utility to parse a timesheet file and print a table of hours by month.
+
+## Timesheet file
+
+We ar eusing a very specific format for the timesheet file. Here is a section showing a couple
+of entries.
+
+    EVENT 2 OF 19
+    Summary:  Project-XYZ
+    Status: none
+    Date: 02/09/2014 to 02/09/2014
+    Time: 18:40:00 to 19:30:00
+    Location:
+    Notes:  Producing style guide
+
+    EVENT 3 OF 19
+    Summary:  Project-XYZ
+    Status: none
+    Date: 02/09/2014 to 02/09/2014
+    Time: 22:30:00 to 23:00:00
+    Location:
+    Notes:  UI discussion
 
 ## Usage
 
-FIXME
+Supply the file location as a parameter.
+
+For example, if using leiningen:
+
+    lein run timetotal /Users/Alice/Desktop/timesheet.txt
 
 ## License
 
-Copyright © 2014 FIXME
+Copyright © 2014 Paul Jackson, m-bryonic Solutions Ltd
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- src/timetotal/core.clj
+++ src/timetotal/core.clj
@@ -1,29 +1,58 @@
 (ns timetotal.core
-  (:require [clj-time.core :as t])
+  (:require [clj-time.core :refer [date-time interval in-minutes month year]]
+            [clojure.pprint :refer [print-table]])
   (:gen-class))
 
-(defn date? [line]
+
+(defn date?
+  "Returns the date if its a date line, otherwise nil"
+  [line]
   (let [groups (re-matches #"^Date:\s+([0-9]{2}/[0-9]{2}/[0-9]{4}) to.*" line)]
     (last groups)))
 
-(defn times? [line]
+(defn times?
+  "Returns the time if its a time line, otherwise nil"
+  [line]
   (let [groups (re-matches #"^Time:\s+([0-9]{2}:[0-9]{2}:[0-9]{2}) to ([0-9]{2}:[0-9]{2}:[0-9]{2})" line)]
     (next groups)))
 
-(defn add-entry [collection entry]
+(defn add-entry
+  "Add the entry to the collection"
+  [collection entry]
   (if (empty? entry)
     collection
     (conj collection entry)))
 
-(defn add-times [entry times]
-  (let [start (first times)
-        end (last times)]
-    (assoc entry :start start :end end)))
+(defn extract-ints
+  "Match using the pattern on the string, returning the matched groups as integers"
+  [pattern str]
+  (map #(Integer. %) (rest (re-matches pattern str))))
 
-(defn fetch-file [path]
+(defn to-date
+  "Create a date-time from the date and time strings"
+  [date-str time-str]
+  (let [dmy (extract-ints #"([0-9]{2})/([0-9]{2})/([0-9]{4})" date-str)
+        hms (extract-ints #"([0-9]{2}):([0-9]{2}):([0-9]{2})" time-str)]
+    (apply date-time (into hms dmy))))
+
+(defn add-times
+  "Add the times to the current entry"
+  [entry times]
+  (let [start (to-date (entry :date)(first times))
+        end (to-date (entry :date)(last times))
+        mth (month start)
+        yr (year start)
+        duration (in-minutes (interval start end))]
+    (assoc entry :start start :end end :duration duration :month mth :year yr)))
+
+(defn fetch-file
+  "Return the file at path as a string"
+  [path]
   (slurp path))
 
-(defn parse-file [file]
+(defn parse-file
+  "Parse the file and return a collection of entry hashes"
+  [file]
   (let [lines (clojure.string/split-lines file)]
     (loop [lines lines
            coll []
@@ -45,17 +74,31 @@
                      curr))))
         (add-entry coll curr)))))
 
-;(def f (load-file "/Users/jacksop/Desktop/Realtime-timesheet.txt"))
-;(parse-file f)
+(defn sum-duration
+  "Sum the fields keyed by duration in the collection"
+  [coll]
+  (->> (for [x coll] (select-keys x [:duration]))
+       (apply merge-with +)
+       :duration))
 
-;(defn -main
-;  [& args]
-;  (println (t/in-minutes (t/interval (t/date-time 1986 10 2) (t/date-time 1986 10 14))))
-;  (println "Hello, World!"))
+(defn group-total
+  "Return totals for a collection of entries"
+  [x]
+  (let [[[year month] group] x
+        total (sum-duration group)]
+    {:year year :month month :hours (format "%.2f" (float (/ total 60)))}))
+
+(defn month-total
+  "Return totals grouped by year and month"
+  [data]
+  (map group-total (group-by (juxt :year :month) data)))
 
 (defn -main
   [& args]
   (let [file (first args)]
     (if file
-      (print (parse-file (fetch-file file)))
+      (print (->> (fetch-file file)
+                  parse-file
+                  month-total
+                  print-table))
       (println "Usage: cmdline FILE-PATH"))))
